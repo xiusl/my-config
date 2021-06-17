688746387dc2ac5c6fa100e084d32c3273bbf51b
xiusl <xiushilin@hotmail.com>
2021-06-17 14:04:17 +0800

    add like mongodb backup

---
+++ mongo_bk/bk.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DB="instance_db"
+
+COLS=("article" "user" "source" "status" "user_action" "user_relation" "feedback")
+
+DT=`date '+%Y%m%d%H%M'`
+
+BKDIR="bk_${DT}"
+
+mkdir $BKDIR
+
+for COL in ${COLS[*]}
+do
+    fname="./$BKDIR/${DB}.${COL}.json"
+    echo "Export ==> ${fname}"
+    (mongoexport -d $DB -c $COL -o $fname) >/dev/null 2>&1
+done
+
+tar -cvf "${BKDIR}.tar" $BKDIR >/dev/null 2>&1
+
+UU="http://v0.api.upyun.com/safekk/db_back/${BKDIR}.tar"
+
+LANG=en_US.UTF-8 
+
+PD=`date -u '+%a, %d %b %Y %T GMT'`
+URI="/safekk/db_back/${BKDIR}.tar"
+FMD=`md5sum ${BKDIR}.tar|cut -d ' ' -f1`
+
+ST="PUT&${URI}&${PD}&${FMD}${UPYS}"
+
+SIGN=`echo -n $ST | openssl sha1 -hmac $UPYP -binary | base64`
+
+curl -X PUT -T "${BKDIR}.tar" $UU \
+         -H "Authorization: UPYUN $UPYU:${SIGN}" \
+         -H "Date: ${PD}"\
+         -H "Content-MD5: $FMD"
+         -H "Content-Secret: $UPYS"
+
---
+++ mongo_bk/readme.md
@@ -0,0 +1,12 @@
+# 对 Like 的数据进行简单备份
+
+
+- 备份文件存储至 upyun
+```
+export UPYU=又拍云操作员账号
+export UPYP=又拍云操作员密码
+export UPYS=文件权限控制
+
+bash bk.sh
+```
+
