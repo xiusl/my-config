f2bc11a9dfd2df2e55a115ab7f92d1c050e8b359
xiusl <xiushilin@hotmail.com>
2021-06-17 14:08:28 +0800

    fix

--- mongo_bk/bk.sh
+++ mongo_bk/bk.sh
@@ -27,13 +27,13 @@ PD=`date -u '+%a, %d %b %Y %T GMT'`
 URI="/safekk/db_back/${BKDIR}.tar"
 FMD=`md5sum ${BKDIR}.tar|cut -d ' ' -f1`
 
-ST="PUT&${URI}&${PD}&${FMD}${UPYS}"
+ST="PUT&${URI}&${PD}&${FMD}"
 
 SIGN=`echo -n $ST | openssl sha1 -hmac $UPYP -binary | base64`
 
 curl -X PUT -T "${BKDIR}.tar" $UU \
          -H "Authorization: UPYUN $UPYU:${SIGN}" \
          -H "Date: ${PD}"\
-         -H "Content-MD5: $FMD"
+         -H "Content-MD5: $FMD" \
          -H "Content-Secret: $UPYS"
 
