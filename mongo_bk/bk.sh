0636234662af11c843afb2c6153dbfd7a21e95cc
xiusl <xiushilin@hotmail.com>
2021-06-22 14:44:02 +0800

    upload progress bar

--- mongo_bk/bk.sh
+++ mongo_bk/bk.sh
@@ -19,21 +19,22 @@ done
 
 tar -cvf "${BKDIR}.tar" $BKDIR >/dev/null 2>&1
 
-UU="http://v0.api.upyun.com/safekk/db_back/${BKDIR}.tar"
+UU="http://v0.api.upyun.com/safekk/db_back_test/${BKDIR}.tar"
 
 LANG=en_US.UTF-8 
 
 PD=`date -u '+%a, %d %b %Y %T GMT'`
-URI="/safekk/db_back/${BKDIR}.tar"
+URI="/safekk/db_back_test/${BKDIR}.tar"
 FMD=`md5sum ${BKDIR}.tar|cut -d ' ' -f1`
 
 ST="PUT&${URI}&${PD}&${FMD}"
 
 SIGN=`echo -n $ST | openssl sha1 -hmac $UPYP -binary | base64`
 
-curl -X PUT -T "${BKDIR}.tar" $UU \
+curl -X PUT --progress-bar -T "${BKDIR}.tar" $UU \
          -H "Authorization: UPYUN $UPYU:${SIGN}" \
          -H "Date: ${PD}"\
          -H "Content-MD5: $FMD" \
-         -H "Content-Secret: $UPYS"
+         -H "Content-Secret: $UPYS" \
+         --progress-bar | test
 
